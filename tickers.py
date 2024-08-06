c29f31161d43ca79b5c8e7486824ed31b96ffa93
Fuhito Suguri <bitwalk@gmail.com>
2024-08-07 07:09:36 +0900

    update

---
+++ tickers.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import wget
+
+if __name__ == '__main__':
+    tse = "https://www.jpx.co.jp/markets/statistics-equities/misc/tvdivq0000001vg2-att/data_j.xls"
+    filename = wget.download(tse)
+    df = pd.read_excel(filename)
+    print(df)
