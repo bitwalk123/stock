85013928f2beab1d6272f6f06f1a92fc02233591
Fuhito Suguri <bitwalk@gmail.com>
2024-08-08 08:23:08 +0900

    update

--- sample_01.py
+++ sample_01.py
@@ -1,12 +1,9 @@
-# Reference
-# https://toukei-lab.com/python_stock
-
 import yfinance as yf
 
-#ターゲットを指定
+# ターゲットを指定
 symbol = '4755.T'
 ticker = yf.Ticker(symbol)
 
-#データを収集
-df = ticker.history(period='5d', interval = '1d')
+# データを収集
+df = ticker.history(period='5d', interval='1d')
 print(df)
