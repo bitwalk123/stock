7342799a0386a1566ae4e1eea735ce4f423e04b5
Fuhito Suguri <bitwalk@gmail.com>
2025-05-17 16:54:12 +0900

    update

--- funcs/agg.py
+++ funcs/agg.py
@@ -68,3 +68,67 @@ def aggregate_up_down_ratio(df: pd.DataFrame, top: int = 100) -> tuple[list, str
     file_html = 'report/%04d/up_down_ratio_%02d-%02d.html' % (dt[0].year, dt[0].month, dt[0].day)
 
     return list_html, file_html
+
+def aggregate_up_down_ratio_weekly(df: pd.DataFrame, top: int = 100) -> tuple[list, str]:
+    # 列名の変更
+    list_col = list(df.columns)
+    col = list(df.columns).index('市場・商品区分')
+    list_col[col] = '区分'
+    df.columns = list_col
+
+    # 「日時」列が 'None' の行を除外
+    df = df[df['日時'] != 'None'].copy()
+
+    # ユニークな日時を抽出
+    dates = df['日時'].unique()
+
+    # 万が一複数の日時が存在した場合、一番新しい日時のデータのみ扱う
+    dt = sorted(dates, reverse=True)
+    df = df[df['日時'] == dt[0]].copy()
+
+    # 「変動率」列で逆ソート
+    df = df.sort_values('変動率', ascending=False).reset_index(drop=True)
+
+    # 順位が判るようにインデックス列から「#」列を作成
+    df['#'] = df.index + 1
+
+    # 「銘柄名」列の文字列を規格化
+    df['銘柄名'] = [unicodedata.normalize('NFKC', s) for s in df['銘柄名']]
+
+    # 「市場・商品区分」列の '（内国株式）' を除外
+    list_category = list()
+    for s in df['区分']:
+        if s == 'グロース（内国株式）':
+            list_category.append('G')
+        elif s == 'スタンダード（内国株式）':
+            list_category.append('S')
+        elif s == 'プライム（内国株式）':
+            list_category.append('P')
+        else:
+            list_category.append(s)
+    df['区分'] = list_category
+
+    list_header = [
+        '#',
+        'コード',
+        '銘柄名',
+        '区分',
+        '33業種区分',
+        '高値',
+        '安値',
+        '変動率',
+        '出来高',
+        '増減',
+    ]
+    df_result = df[list_header].copy()
+    list_col_format = ['int', 'code', 'str', 'str', 'str', 'int', 'int', 'float', 'int', 'str']
+
+    # 念のため
+    if len(df_result) < top:
+        top = len(df_result)
+
+    # HTML に変換
+    list_html = df_to_html(df_result.iloc[0:top], list_col_format)
+    file_html = 'report/%04d/up_down_ratio_weekly_%02d-%02d.html' % (dt[0].year, dt[0].month, dt[0].day)
+
+    return list_html, file_html
---
+++ up_down_ratio_weekly.py
@@ -0,0 +1,59 @@
+import time
+
+import yfinance as yf
+
+from funcs.agg import aggregate_up_down_ratio_weekly
+from funcs.tse import get_ticker_list
+
+if __name__ == '__main__':
+    # 対象銘柄リストの取得
+    df = get_ticker_list()
+
+    for r in range(len(df)):
+        code = df.at[r, 'コード']
+        symbol = '%s.T' % code
+        ticker = yf.Ticker(symbol)
+        try:
+            df_ticker = ticker.history(period='1wk')
+        except Exception as e:
+            print(e)
+            # ３分待って再試行
+            time.sleep(180)
+            df_ticker = ticker.history(period='1wk')
+
+        if len(df_ticker) == 0:
+            continue
+
+        v_open = df_ticker['Open'].iloc[0]
+        v_high = df_ticker['High'].iloc[0]
+        v_low = df_ticker['Low'].iloc[0]
+        v_close = df_ticker['Close'].iloc[0]
+        v_volume = df_ticker['Volume'].iloc[0]
+
+        # 始値と終値の平均が 100 円未満の銘柄は除外
+        if (v_open + v_close) / 2 < 100:
+            continue
+
+        # 出来高が 1,000,000 株未満の銘柄は除外
+        if v_volume < 10000000:
+            continue
+
+        df.at[r, '高値'] = v_high
+        df.at[r, '安値'] = v_low
+        df.at[r, '変動率'] = (v_high - v_low) * 2 / (v_open + v_close)
+        df.at[r, '出来高'] = v_volume
+        df.at[r, '日時'] = df_ticker.index[0]
+
+        if v_open > v_close:
+            df.at[r, '増減'] = '▼'
+        elif v_open < v_close:
+            df.at[r, '増減'] = '△'
+        else:
+            df.at[r, '増減'] = '-'
+
+    # 変動率の集計
+    list_html, file_html = aggregate_up_down_ratio_weekly(df)
+
+    # 結果の出力
+    with open(file_html, mode='w') as f:
+        f.writelines(list_html)
