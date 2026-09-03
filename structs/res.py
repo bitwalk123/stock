8f98a087ad0d4eeb5d7b59b5254f977ceb1d8d2c
Fuhito Suguri <bitwalk@gmail.com>
2026-09-03 15:46:55 +0900

    update

--- structs/res.py
+++ structs/res.py
@@ -2,7 +2,7 @@ import pandas as pd
 
 
 class AppRes:
-    tse = "https://www.jpx.co.jp/markets/statistics-equities/misc/tvdivq0000001vg2-att/data_j.xls"
+    tse = "https://www.jpx.co.jp/markets/statistics-equities/misc/tvdivq0000001vg2-att/data_j.xlsx"
 
     def getJPXTickerList(self) -> pd.DataFrame:
         return pd.read_excel(self.tse)
